e57f0b377d29324d366cadb0923485ed6ec7e168
Jason Chen <jasonzchen@gmail.com>
2025-12-29 15:12:02 -0800

    Finalize refactor

--- test.py
+++ test.py
@@ -10,9 +10,10 @@ import random
 
 
 def addition(a,b):
+    '''Insert Docstring here'''
     return a+b
 
 
 
 
-print(addition(random.randint(0,100), random.randint(0, 100)))
+print(addition(random.randint(0,110), random.randint(0, 100)))

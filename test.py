2f5f4fd14dc767c6840a88ccd08dc43d4c4d5ec7
Jason Chen <jasonzchen@gmail.com>
2025-12-29 15:04:45 -0800

    First Commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from datetime import datetime
+from pytz import timezone
+import pytz
+from datetime import timedelta
+
+
+
+
+
+def addition(a,b):
+    return a+b
+
+
+import random
+
+print(addition(random.randint(0,100), random.randint(0, 100)))
